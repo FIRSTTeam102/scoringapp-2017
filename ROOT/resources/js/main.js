1ab3707ffa49417e8f49751e4cd860e7f91d63c4
Gearheads5 <firstteam102@gmail.com>
2017-02-10 20:21:10 -0500

    Deleted finishalliancecallchoosematch function. Also, on previous commit, added code in auth.jsp that checks if the user is logged in; and sets isLoggedIn on login.jsp.

--- ROOT/resources/js/main.js
+++ ROOT/resources/js/main.js
@@ -51,63 +51,6 @@ function remove(){
 	$('#self-destruct').remove();
 }
 
-function finishAlCallChoosematch() {
-	$("CompetitionForm").on("submit", function(e) {
-		   //to finish alliance
-		   event.preventDefault();//Stops refresh
-		   console.log("beginning finishAlliance part of finishAlCallChoosematch");
-		   var rdoAlliance = $("input[name='rdoAlliance']:checked").val();//A String either "Red" or "Blue"
-		   $.post("alliance-finish.jsp",
-			{
-			   selAlliance: rdoAlliance 
-			},
-			
-			function(data, status, xhr) {
-				//Sets the content div's contents to whatever the jsp page has on it.
-				//$("#content").html(data); 
-				if(status == "success"){
-					$("#content").append(data.trim()); //alliance-finish will return scripts
-					
-				}else if(status == "error"){
-					alert("An error occurred.");
-					error("Error:" + xhr.status);
-				}
-			});
-
-		//Start move to choosematch
-		console.log("Requesting Choosematch from within finishAlCallChoosematch...");
-	   $.post("choosematch.jsp",
-		{
-		
-		},
-		
-		function(data, status, xhr) {
-			//Sets the content div's contents to whatever the jsp page has on it.
-			//$("#content").html(data); 
-			if(status == "success"){
-				console.log("Successfully loaded Choosematch");
-				if(currentPage != "#choosematch"){
-					$(currentPage).hide(0, function(){
-						
-						$("#choosematch").html(data.trim());
-						currentPage = "#choosematch";
-					});
-				}else{
-					$("#choosematch").html(data.trim());
-				}
-				
-			}else if(status == "error"){
-				alert("A choosematch error occurred.");
-				error("Error:" + xhr.status);
-			}
-		});
-	});
-}
-// Tried to condense these into one, WORKING function. Does not right now.
-
-
-
-
 function requestAllianceSelection(){
 	console.log("Requesting allianceselection...");
 	$.post("alliance.jsp",
